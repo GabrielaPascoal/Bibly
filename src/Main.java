c5502dfacdd5f5a177da6bc399f2fdb49ff9523a
Felipe Silva <f.filipisilva13@hotmail.com>
2021-08-20 12:51:26 -0300

    correção-livroVO-ano

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,6 @@ import model.VO.LivroVO;
 class Main {
   public static void main(String args[]) {
 
-	  Scanner ler = new Scanner(System.in);
-	  LivroVO livro = new LivroVO();
-	  
-	  System.out.println("Digite o ano do livro: ");
-	  int liv = ler.nextInt();
-	  livro.setAno(liv);
-	  
-
 	 	  
   }
 }
