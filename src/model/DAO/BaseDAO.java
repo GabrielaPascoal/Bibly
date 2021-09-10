56627e32f58e1ad8514046d59a0dd1ee0b0e628d
maicond75 <maicondouglas75@hotmail.com>
2021-09-10 17:13:19 -0300

    feat/implementacao BaseDAO

---
+++ src/model/DAO/BaseDAO.java
@@ -0,0 +1,17 @@
+package model.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseDAO {
+  private static Connection connection = null;
+
+  public static Connection getConnection() throws SQLException {
+    String url = "jdbc:postgresql://localhost:5432/bibly";
+    String user = "postgres";
+    String senha = "postgres";
+
+    return connection == null ? DriverManager.getConnection(url, user, senha) : connection;
+  }
+}
