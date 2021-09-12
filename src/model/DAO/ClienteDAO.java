5e0f87e53d33477ada12c40f7786d63181be8293
Gabi Pascoal <gabrielaopascoal@outlook.com>
2021-09-12 01:24:17 -0300

    Inserir, deletar, listar, alterar e buscar

---
+++ src/model/DAO/ClienteDAO.java
@@ -0,0 +1,136 @@
+package model.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.VO.ClienteVO;
+
+public class ClienteDAO extends BaseDAO {
+	
+	public void inserir(ClienteVO vo) throws SQLException {
+	Connection connection = getConnection();
+	String query = "INSERT INTO clientes (nome, cpf, endereco, celular) VALUES(?, ?, ?, ?)";
+	PreparedStatement preparedStatement = connection.prepareStatement(query);
+	
+	preparedStatement.setString(1, vo.getNome());
+	preparedStatement.setString(2, vo.getCpf());
+	preparedStatement.setString(3, vo.getEndereco());
+	preparedStatement.setInt(4, vo.getCelular());
+	preparedStatement.execute();
+	
+	}
+	// não está deletando
+	public void deletarPorID(ClienteVO vo) throws SQLException {
+		
+		Connection connection = getConnection();
+		String query = "DELETE FROM clientes WHERE id = ?";
+		try {
+		    PreparedStatement preparedStatement = connection.prepareStatement(query);
+		    preparedStatement.setInt(1, vo.getId());
+		    preparedStatement.executeUpdate();
+		    
+		} catch (SQLException e) {
+			e.printStackTrace(); 
+		}
+		
+		
+	}
+	
+	public List<ClienteVO> listarClientes() throws SQLException {
+		
+		Connection connection = getConnection();
+		String query = "SELECT * FROM clientes";
+		List<ClienteVO> clientes = new ArrayList <ClienteVO>();
+		
+		try {
+		    Statement st = connection.createStatement();
+		    ResultSet result = st.executeQuery(query);
+		    while(result.next()) {
+		    	ClienteVO vo = new ClienteVO();
+		    	vo.setId(result.getInt("id"));
+		    	vo.setNome(result.getString("nome"));
+		    	vo.setCpf(result.getString("cpf"));
+		    	vo.setEndereco(result.getString("endereco"));
+		    	
+		    	clientes.add(vo);    	
+			
+		}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return clientes;
+			
+		
+	}
+	public void alterarCliente(ClienteVO vo) throws SQLException {
+		Connection connection = getConnection();
+		String query = "UPDATE clientes SET nome = ?, cpf = ?, endereco = ?";
+		PreparedStatement preparedStatement;
+		try {
+		    preparedStatement = connection.prepareStatement(query);
+		    preparedStatement.setString(1, vo.getNome());
+		    preparedStatement.setString(2, vo.getCpf());
+		    preparedStatement.setString(1, vo.getEndereco());
+		    preparedStatement.executeUpdate();
+		    
+		} catch (SQLException e) {
+			e.printStackTrace(); 
+		}
+		
+	}
+
+	public ResultSet buscarPorNome(ClienteVO vo) {
+		String query = "SELECT * FROM clientes WHERE nome = ?";
+		PreparedStatement preparedStatement;
+		ResultSet result = null;
+				
+ 		try {
+			preparedStatement = getConnection().prepareStatement(query);
+			preparedStatement.setString(1, vo.getNome());
+			result = preparedStatement.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;		
+		
+		
+	}
+	
+	public ResultSet buscarPorCpf(ClienteVO vo) {
+		String query = "SELECT * FROM clientes WHERE cpf = ?";
+		PreparedStatement preparedStatement;
+		ResultSet result = null;
+				
+ 		try {
+			preparedStatement = getConnection().prepareStatement(query);
+			preparedStatement.setString(1, vo.getCpf());
+			result = preparedStatement.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;	
+		
+	}
+	
+
+    public ResultSet buscarPorEndereco(ClienteVO vo) {
+	   String query = "SELECT * FROM clientes WHERE endereco = ?";
+	   PreparedStatement preparedStatement;
+	   ResultSet result = null;
+	 		
+		try {
+		    preparedStatement = getConnection().prepareStatement(query);
+		    preparedStatement.setString(1, vo.getEndereco());
+		    result = preparedStatement.executeQuery();
+	    } catch (SQLException e) {
+		    e.printStackTrace();
+	    }
+	    return result;
+	  
+    }
+}
