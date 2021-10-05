b06942865c092fb0ed725d0dde6c63c96f8543c1
maicond75 <maicondouglas75@hotmail.com>
2021-10-05 09:15:21 -0300

    adicionando método closeConnection

--- src/model/DAO/BaseDAO.java
+++ src/model/DAO/BaseDAO.java
@@ -6,12 +6,22 @@ import java.sql.SQLException;
 
 public abstract class BaseDAO<VO> implements BaseInterDAO<VO> {
   private static Connection connection = null;
+  private static final String url = "jdbc:postgresql://localhost:5432/bibly";
+  private static final String user = "postgres";
+  private static final String senha = "postgres";
 
   public static Connection getConnection() throws SQLException {
-    String url = "jdbc:postgresql://localhost:5432/bibly";
-    String user = "postgres";
-    String senha = "postgres";
+    try {
+      return connection == null ? DriverManager.getConnection(url, user, senha) : connection;
+    } catch (SQLException erro) {
+      erro.printStackTrace();
+      return connection;
+    }
+  }
 
-    return connection == null ? DriverManager.getConnection(url, user, senha) : connection;
+  public static void closeConnection() throws SQLException {
+    if (connection != null) {
+      connection.close();
+    }
   }
 }
