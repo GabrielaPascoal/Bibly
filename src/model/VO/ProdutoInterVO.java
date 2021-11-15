1cbed316f8d12432fd0e2e643652f03dd72403b0
maicond75 <maicondouglas75@hotmail.com>
2021-11-15 18:06:25 -0300

    implementaçcao interface dos produtos

---
+++ src/model/VO/ProdutoInterVO.java
@@ -0,0 +1,24 @@
+package model.VO;
+
+public interface ProdutoInterVO {
+  int id = 0;
+  String titulo = "";
+  int quantidade = 0;
+  double valor = 0;
+
+  public int getId();
+
+  public void setId(int id);
+
+  public String getTitulo();
+
+  public void setTitulo(String titulo);
+
+  public int getQuantidade();
+
+  public void setQuantidade(int quantidade);
+
+  public double getValor();
+
+  public void setValor(double valor);
+}
